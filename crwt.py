20ac5dc169f41e8466fb8de54fe2d042bb3c45f1
Sarvesh Sathish <sarveshsathish.2004@gmail.com>
2020-09-26 01:23:58 -0500

    Update crwt.py

--- crwt.py
+++ crwt.py
@@ -1,1 +1,31 @@
-print('hi')
+import matplotlib.pyplot as plt
+import networkx as nx
+
+G = nx.Graph()
+
+count = 0
+G.add_edge(1, 2, weight=10)
+G.add_edge(3,4,weight=0.4)
+G.add_edge(2,3)
+
+elarge = [(u, v) for (u, v, d) in G.edges(data=True) if d["weight"] > 0.5]
+esmall = [(u, v) for (u, v, d) in G.edges(data=True) if d["weight"] <= 0.5]
+
+pos = nx.spring_layout(G)  # positions for all nodes
+
+# nodes
+nx.draw_networkx_nodes(G, pos, node_size=700)
+
+# edges
+nx.draw_networkx_edges(G, pos, edgelist=elarge, width=6)
+nx.draw_networkx_edges(
+    G, pos, edgelist=esmall, width=6, alpha=0.5, edge_color="b", style="dashed"
+)
+
+# labels
+nx.draw_networkx_labels(G, pos, font_size=20, font_family="sans-serif")
+
+plt.axis("off")
+plt.show()
+
+print(G.has_edge(1,3))
